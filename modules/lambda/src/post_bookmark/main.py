342283f8902ac4c9ccb52d7aae6e1eea5f6ddfcd
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-03-12 23:16:17 +0900

    linter適用 & response body追加

--- modules/lambda/src/post_bookmark/main.py
+++ modules/lambda/src/post_bookmark/main.py
@@ -7,11 +7,14 @@ from uuid import uuid4
 
 bookmark_dynamoDB_table_name = os.environ['bookmark_dynamoDB_table_name']
 
+
 def utc_to_jst(timestamp_utc: str) -> str:
-    datetime_utc =  datetime.strptime(timestamp_utc, '%Y-%m-%dT%H:%M%z')
+    datetime_utc = datetime.strptime(timestamp_utc, '%Y-%m-%dT%H:%M%z')
     datetime_jst = datetime_utc.astimezone(timezone(timedelta(hours=+9)))
-    timestamp_jst =  datetime.strftime(datetime_jst, '%Y-%m-%dT%H:%M' + '+09:00')
+    timestamp_jst = datetime.strftime(
+        datetime_jst, '%Y-%m-%dT%H:%M' + '+09:00')
     return timestamp_jst
+
 
 def get_current_jst() -> str:
     date_now = datetime.now(tz=timezone.utc)
@@ -19,7 +22,7 @@ def get_current_jst() -> str:
     return utc_to_jst(date_now_iso)
 
 
-def put_item(userName: str, bookmark_url: str, tag_id_list: List[str]) -> None:
+def put_item(userName: str, bookmark_id: str, bookmark_url: str, tag_id_list: List[str], registered_date: str) -> None:
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(bookmark_dynamoDB_table_name)
     response = table.put_item(
@@ -27,20 +30,32 @@ def put_item(userName: str, bookmark_url: str, tag_id_list: List[str]) -> None:
             'userName': userName,
             'bookmarkID': str(uuid4()),
             'bookmarkURL': bookmark_url,
-            'registeredDate': get_current_jst(),
+            'registeredDate': registered_date,
             'tagIDs': tag_id_list
         }
     )
     return response
+
 
 def lambda_handler(event: dict, context):
     userName: str = event['pathParameters']['userName']
     request_body: dict = json.loads(event['body'])
     bookmark_url: str = request_body['bookmarkURL']
     tag_id_list: List[str] = request_body['tagsIDs']
+    bookmark_id: str = str(uuid4())
+    registered_date: str = get_current_jst()
 
-    put_item(userName=userName, bookmark_url=bookmark_url, tag_id_list=tag_id_list)
+    put_item(userName=userName, bookmark_id=bookmark_id, bookmark_url=bookmark_url,
+             tag_id_list=tag_id_list, registered_date=registered_date)
+
+    response_body = {
+        'data': {
+            'bookmarkID': bookmark_id,
+            'registeredDate': registered_date
+        }
+    }
 
     return {
         'statusCode': 201,
+        'body': json.dumps(response_body)
     }
