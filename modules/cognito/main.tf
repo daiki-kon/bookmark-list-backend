f7c56715216964235210b183b8980a04f6c81457
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-03-15 23:56:12 +0900

    fix cognito

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -1,6 +1,40 @@
 resource "aws_cognito_user_pool" "user_manage" {
   name = var.app_name
 
+  auto_verified_attributes = [
+    "email",
+  ]
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  verification_message_template {
+    default_email_option  = "CONFIRM_WITH_CODE"
+    email_message         = " 検証コードは {####} です。"
+    email_subject         = " 検証コード"
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
+    }
+  }
+
+
   password_policy {
     minimum_length                   = 6
     require_lowercase                = false
@@ -9,30 +43,27 @@ resource "aws_cognito_user_pool" "user_manage" {
     require_uppercase                = false
     temporary_password_validity_days = 7
   }
+
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
 }
 
 resource "aws_cognito_user_pool_client" "user_manage_secret" {
   name = var.app_name
 
-  generate_secret = true
-  user_pool_id    = aws_cognito_user_pool.user_manage.id
-}
-
-resource "aws_cognito_user_pool_client" "user_manage" {
-  name = var.app_name
-
   generate_secret = false
   user_pool_id    = aws_cognito_user_pool.user_manage.id
+
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+  ]
 }
 
 resource "aws_cognito_identity_pool" "user_manage" {
   identity_pool_name               = var.app_name
-  allow_unauthenticated_identities = true
-
-  cognito_identity_providers {
-    client_id     = aws_cognito_user_pool_client.user_manage.id
-    provider_name = aws_cognito_user_pool.user_manage.endpoint
-  }
+  allow_unauthenticated_identities = false
 
   cognito_identity_providers {
     client_id     = aws_cognito_user_pool_client.user_manage_secret.id
--- modules/cognito/output.tf
+++ modules/cognito/output.tf
@@ -1,3 +1,3 @@
 output "cognito_user_pool_id" {
-  value = aws_cognito_user_pool_client.user_manage.id
+  value = aws_cognito_user_pool_client.user_manage_secret.id
 }
