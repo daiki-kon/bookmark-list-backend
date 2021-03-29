0382e6216950027e313aa2fab0b5ad81743c720e
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-03-29 22:26:15 +0900

    fmt'

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -17,9 +17,9 @@ resource "aws_cognito_user_pool" "user_manage" {
   }
 
   verification_message_template {
-    default_email_option  = "CONFIRM_WITH_CODE"
-    email_message         = " 検証コードは {####} です。"
-    email_subject         = " 検証コード"
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_message        = " 検証コードは {####} です。"
+    email_subject        = " 検証コード"
   }
 
   schema {
