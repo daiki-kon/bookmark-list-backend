3f7af7494bd6dc5b238b76eb3f1b52ffa1f039c8
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-04-02 01:16:06 +0900

    ecr permission

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -6,3 +6,36 @@ resource "aws_ecr_repository" "amplify_console" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository_policy" "amplify_console" {
+  repository = aws_ecr_repository.amplify_console.name
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "ReadOnlyPermissions",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "amplify.amazonaws.com"
+      },
+      "Action": [
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:BatchGetImage",
+        "ecr:DescribeImageScanFindings",
+        "ecr:DescribeImages",
+        "ecr:DescribeRepositories",
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetLifecyclePolicy",
+        "ecr:GetLifecyclePolicyPreview",
+        "ecr:GetRepositoryPolicy",
+        "ecr:ListImages",
+        "ecr:ListTagsForResource"
+      ]
+    }
+  ]
+}
+EOF
+}
