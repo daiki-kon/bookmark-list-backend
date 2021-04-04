eed5cc76510e00fc79cfedf6d70f4a4175d80c72
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-04-04 21:23:54 +0900

    get tags opstions作成

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -17,7 +17,8 @@ resource "aws_api_gateway_deployment" "bookmark_list" {
     aws_api_gateway_integration.post_tag,
     aws_api_gateway_integration.put_tag,
     aws_api_gateway_integration.get_tags,
-    aws_api_gateway_integration.ogp_options_mock
+    aws_api_gateway_integration.ogp_options_mock,
+    aws_api_gateway_integration.get_tags_options_mock,
   ]
 
   triggers = {
@@ -360,4 +361,56 @@ resource "aws_api_gateway_integration" "get_tags" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.get_tags_lambda_function_invoke_arn
+}
+
+resource "aws_api_gateway_method" "get_tags_options" {
+  rest_api_id   = aws_api_gateway_rest_api.bookmark_list.id
+  resource_id   = aws_api_gateway_resource.tags.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "get_tags_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.bookmark_list.id
+  resource_id = aws_api_gateway_resource.tags.id
+  http_method = aws_api_gateway_method.get_tags_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "get_tags_options" {
+  rest_api_id = aws_api_gateway_rest_api.bookmark_list.id
+  resource_id = aws_api_gateway_resource.tags.id
+  http_method = aws_api_gateway_method.get_tags_options.http_method
+  status_code = aws_api_gateway_method_response.get_tags_options_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_integration" "get_tags_options_mock" {
+  rest_api_id = aws_api_gateway_rest_api.bookmark_list.id
+  resource_id = aws_api_gateway_resource.tags.id
+  http_method = aws_api_gateway_method.get_tags_options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "statusCode": 200
+}
+EOF
+  }
 }
